bbc4c03ab6f29ee3bd93b74d7be22f114e47543a
michiel-j <72069547+michiel-j@users.noreply.github.com>
2021-11-10 13:40:59 +0100

    Replaced main.c with cleaner version

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -17,130 +17,51 @@
   ******************************************************************************
   */
 /* USER CODE END Header */
+
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
-
-/* USER CODE END Includes */
-
-/* Private typedef -----------------------------------------------------------*/
-/* USER CODE BEGIN PTD */
-
-/* USER CODE END PTD */
-
-/* Private define ------------------------------------------------------------*/
-/* USER CODE BEGIN PD */
-/* USER CODE END PD */
-
-/* Private macro -------------------------------------------------------------*/
-/* USER CODE BEGIN PM */
-
-/* USER CODE END PM */
-
-/* Private variables ---------------------------------------------------------*/
 I2S_HandleTypeDef hi2s3;
 DMA_HandleTypeDef hdma_spi3_rx;
 
-/* USER CODE BEGIN PV */
 
-/* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_I2S3_Init(void);
 static void MX_DMA_Init(void);
-/* USER CODE BEGIN PFP */
 
-/* USER CODE END PFP */
 
-/* Private user code ---------------------------------------------------------*/
-/* USER CODE BEGIN 0 */
-
-/* USER CODE END 0 */
-
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
   HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
 
   /* Configure the system clock */
   SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_I2S3_Init();
   MX_DMA_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
 
   /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
   HAL_StatusTypeDef retval; // return value
 
-  // 1
-//  uint16_t data[2] = {0, 0};
-//  int16_t data_signed[2] = {0, 0};
-
-  // 2
-//  int32_t data[1000];
-//  int16_t data_signed[1000];
-
-//  uint16_t data[100];
   volatile int16_t data[500] = {0};
   int16_t data_shifted[500];
 
   while (1)
   {
-
-//	  HAL_I2S_Receive(&hi2s3, data, 4*250, 1000);
-//	  HAL_I2S_Receive_DMA(&hi2s3, data, 4*250);
-//	  HAL_I2S_Receive_DMA(&hi2s3, data, 4);
-
-//	  for(short i=0; i<500; i++){
-//		  HAL_I2S_Receive_DMA(&hi2s3, &data[i], 1);
-////		  i++;
-//	  }
-//	  HAL_I2S_Receive_DMA(&hi2s3, data, 100);
 	  retval = HAL_I2S_Receive_DMA(&hi2s3, data, 500);
 //	  for(short i=0; i<500; i++){
 //		  data_shifted[i] = data[i] >> 14;
 //	  }
 
   HAL_Delay(1000);
-
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
   }
-  /* USER CODE END 3 */
 }
 
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
